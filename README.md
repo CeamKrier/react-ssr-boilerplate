ab7cc8aaab1a55c02b35fbfd5c9dc05694ce7698
Emre Çakır <ceamkrier@gmail.com>
2020-04-05 23:47:39 +0300

    init: basics of the boilerplate implemented

---
+++ README.md
@@ -0,0 +1,22 @@
+This boilerplate constructed upon create-react-app and utilizing server-side rendering approach.
+
+## Why
+
+If you would love to use the server-side rendering on your react application yet don't want to struggle with a new framework then here the solution for ya.
+This is a most basic implementation that would get you up an running for the SSR so no routing etc. included.
+
+## The flow
+
+- Development: All the benefits of regular create-react-app. Simply run `npm run dev`
+
+- Production: Generate the build of your react app with the `npm run build` then SSR application with `npm start`
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Thanks :raised_hands:
+
+## License
+
+[MIT](<[https://choosealicense.com/licenses/mit/](https://choosealicense.com/licenses/mit/)>)
---
+++ server/server.js
@@ -0,0 +1,38 @@
+import path from 'path';
+import fs from 'fs';
+import compression from 'compression';
+import express from 'express';
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+
+import App from '../src/components/App';
+
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(compression());
+
+app.use(
+	express.static(path.resolve(__dirname, '..', 'build'), { maxAge: '30d' })
+);
+
+const serverRenderer = (req, res, next) => {
+	fs.readFile(path.resolve('./build/index.html'), 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+			return res.status(500).send('An error occurred');
+		}
+		return res.send(
+			data.replace(
+				'<div id="root"></div>',
+				`<div id="root">${ReactDOMServer.renderToString(<App />)}</div>`
+			)
+		);
+	});
+};
+
+app.get('*', serverRenderer);
+
+app.listen(PORT, () => {
+	console.log(`Server is up and running on port ${PORT}`);
+});
