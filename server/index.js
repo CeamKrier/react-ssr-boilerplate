6bd78ab0e68797c43eb1abbc28b3723593a56fcd
Emre Çakır <ceamkrier@gmail.com>
2020-04-11 16:09:48 +0300

    chore: comments added for the tricky parts inside the server implementation

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
+// Ignores the style imports while renderin on the node environment
 require('ignore-styles');
 
+// Registers necessary babel transpilers
 require('@babel/register')({
 	ignore: [/(node_modules)/],
 	presets: ['@babel/preset-env', '@babel/preset-react'],
 });
 
+/**
+ * Registering the required transpilers and running the server
+ * from the same file is failing. Thus, invoking the server from here.
+ */
 require('./server');
--- server/server.js
+++ server/server.js
@@ -2,15 +2,26 @@ import path from 'path';
 import compression from 'compression';
 import express from 'express';
 import HomeRoute from './routes/home';
+import Config from './config';
 
 const PORT = process.env.PORT || 8080;
 const app = express();
 
 app.use(compression());
 
+/**
+ * index.html file presents inside the build folder.
+ * When the browser sees it, it directly renders the page with the content inside of it.
+ * That prevents routes to getting triggered.
+ * Thus, `{ index: false }` option prevents the share of the file and routes will get triggered.
+ *
+ * `disable('x-powered-by')` hides the technology used on our server. Some says that prevents
+ * 	attackers to obtain necessary info. Not sure, but fine to add. I took that from the source
+ *  code of the Razzle.
+ */
 app
 	.disable('x-powered-by')
-	.use(express.static(path.resolve(__dirname, '..', 'build'), { index: false }))
+	.use(express.static(path.resolve(Config.BUILD_FOLDER_PATH), { index: false }))
 	.get('/', HomeRoute);
 
 app.listen(PORT, () => {
